1552322623bd1f3b8be89e819737d33ff6e18877
Kenji.N <1314webdev@gmail.com>
2022-06-19 22:03:57 -0300

    added active page indicator on the nav menu

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // js for hamburger menu and possible dark mode
 // remember to use filter for images, so they get a bit darker and easier to look at.
 // #121212 for black
-// make a modal 
+// make a modal
 
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   menu.classList.toggle("menu-open");
+});
+
+const activeMenu = window.location.pathname;
+const menuItems = document.querySelectorAll(".menu__text").forEach((item) => {
+  if (item.href.includes(`${activeMenu}`)) {
+    item.classList.add("active-menu");
+  }
 });
