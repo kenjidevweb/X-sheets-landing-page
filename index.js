cb0793abb604c0f6b386dbfc34c980194dcb27bb
Kenji.N <1314webdev@gmail.com>
2022-07-03 17:22:51 -0300

    improvements on the modal

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   menu.classList.toggle("menu-open");
 });
 
-const didAnimationEnd = document.querySelector(".neon span");
 const animatedItems = document.querySelectorAll(".neon span");
 
 function equalizeDelay() {
@@ -13,8 +13,10 @@ function equalizeDelay() {
     item.setAttribute("id", "animation-phase-2");
   });
 }
-didAnimationEnd.addEventListener("animationend", equalizeDelay);
 
+animatedItems[0].addEventListener("animationend", equalizeDelay);
+
+const pageBody = document.querySelector("body");
 const ctaBtn = document.querySelectorAll(".cta");
 const closeBtn = document.querySelector(".close");
 const popupModal = document.querySelector(".modal");
@@ -24,15 +26,17 @@ const preorderForm = document.querySelector(".preorder-form");
 ctaBtn.forEach((button) => {
   button.addEventListener("click", () => {
     popupModal.classList.add("active");
+    pageBody.classList.add("modal-open");
   });
 });
 
 closeBtn.addEventListener("click", () => {
   popupModal.classList.remove("active");
   submitted.classList.remove("active");
+  pageBody.classList.remove("modal-open");
 });
 
 preorderForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+  event.preventDefault(); /* This will stop the form from being submitted, so the user isnt redirected to another page */
   submitted.classList.add("active");
 });
