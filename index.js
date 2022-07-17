6d13424cbd4dec46f408c3555d26dde295881361
Kenji.N <1314webdev@gmail.com>
2022-07-17 13:38:16 -0300

    Fixed layout on fold phones

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
@@ -5,6 +6,8 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   menu.classList.toggle("menu-open");
 });
+
+// animated neon text
 
 const animatedItems = document.querySelectorAll(".neon span");
 
@@ -16,7 +19,8 @@ function equalizeDelay() {
 
 animatedItems[0].addEventListener("animationend", equalizeDelay);
 
-const pageBody = document.querySelector("body");
+// pop up modal
+
 const ctaBtn = document.querySelectorAll(".cta");
 const closeBtn = document.querySelector(".close");
 const popupModal = document.querySelector(".modal");
@@ -26,17 +30,17 @@ const preorderForm = document.querySelector(".preorder-form");
 ctaBtn.forEach((button) => {
   button.addEventListener("click", () => {
     popupModal.classList.add("active");
-    pageBody.classList.add("modal-open");
+    document.body.classList.add("modal-open");
   });
 });
 
 closeBtn.addEventListener("click", () => {
   popupModal.classList.remove("active");
   submitted.classList.remove("active");
-  pageBody.classList.remove("modal-open");
+  document.body.classList.remove("modal-open");
 });
 
 preorderForm.addEventListener("submit", (event) => {
-  event.preventDefault(); /* This will stop the form from being submitted, so the user isnt redirected to another page */
+  event.preventDefault();
   submitted.classList.add("active");
 });
